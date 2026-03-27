ab017cf435817f22b23808b5e8605779d58cff17
Asmitta-01 <tiwabrayan@gmail.com>
2026-03-27 16:30:21 +0100

    Bump plugin version to 0.1.1

--- setup.php
+++ setup.php
@@ -32,7 +32,7 @@
  */
 
 /** @phpstan-ignore theCodingMachineSafe.function (safe to assume this isn't already defined) */
-define('PLUGIN_TICKETSSTATISTICS_VERSION', '0.1.0');
+define('PLUGIN_TICKETSSTATISTICS_VERSION', '0.1.1');
 
 // Minimal GLPI version, inclusive
 /** @phpstan-ignore theCodingMachineSafe.function (safe to assume this isn't already defined) */
